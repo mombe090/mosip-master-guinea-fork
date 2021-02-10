259c011e3085b56de077b3cb60142d84cdef2720
av <ankitvaishnav.iitb1991@gmail.com>
2021-02-10 15:23:26 +0530

    guinea tested

--- deployment/sandbox-v2/test/regproc/gen_hash.py
+++ deployment/sandbox-v2/test/regproc/gen_hash.py
@@ -1,13 +1,13 @@
-#!/bin/python3.6
-# This scripts generates packet hash and OSI hash. 
+# This scripts generates packet hash and OSI hash.
 # Usage (example):
 # ./gen_hash.py data/packet/packet1/unencrypted/id/packet_meta_info.json 
 
 import sys
 import json
 import os
-import hashlib 
+import hashlib
 import glob
+
 
 def gen_path_dict(base):
     # Generate file path dictionary
@@ -17,6 +17,7 @@ def gen_path_dict(base):
         path_dict[os.path.basename(f).split('.')[0]] = f
     return path_dict
 
+
 def gen_hash(j, label, path_dict):
     '''
     j: meta info json
@@ -25,11 +26,11 @@ def gen_hash(j, label, path_dict):
     if len(j['identity'][label]) == 0:
         return ''
 
-    files =  [x['value'] for x in j['identity'][label]]
+    files = [x['value'] for x in j['identity'][label]]
     files = [x for f in files for x in f]  # Flatten 
     h = hashlib.sha256()
     for f in files:
-        h.update(open(path_dict[f], 'rb').read()) 
+        h.update(open(path_dict[f], 'rb').read())
 
     return h.hexdigest().upper()
 
@@ -41,7 +42,7 @@ def main():
     meta_j = json.load(fp)
 
     base = os.path.dirname(meta_path)
-    path_dict =  gen_path_dict(base)
+    path_dict = gen_path_dict(base)
     hash1 = gen_hash(meta_j, 'hashSequence1', path_dict)
     if hash1:
         open(os.path.join(base, 'packet_data_hash.txt'), 'wb').write(hash1.encode())
@@ -49,12 +50,7 @@ def main():
     hash2 = gen_hash(meta_j, 'hashSequence2', path_dict)
     if hash2:
         open(os.path.join(base, 'packet_operations_hash.txt'), 'wb').write(hash2.encode())
-    
-if __name__== '__main__':
+
+
+if __name__ == '__main__':
     main()
-
-
-
-
-
-
