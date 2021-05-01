ccfe4459c245ab3b4d6c3c03404e332df0f9fbc4
mombe090 <yayamombeya090@gmail.com>
2021-05-01 19:02:30 +0000

    drop unused mapping

--- deployment/production/terraform/prod-env/extra_vm.sh
+++ deployment/production/terraform/prod-env/extra_vm.sh
@@ -23,4 +23,15 @@ service sshd restart
 systemctl stop firewalld
 systemctl disable firewalld
 
+# INSTALL DOCKER
+sudo yum install -y yum-utils
 
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker centos
